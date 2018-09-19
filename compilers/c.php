f2d3f014c25983c309a48c1af91b2e11fdef0450
ayush115 <33548141+ayush115@users.noreply.github.com>
2018-09-20 00:41:12 +0530

    Update c.php

--- compilers/c.php
+++ compilers/c.php
@@ -1,70 +1,88 @@
-<?php 
-$code=$_POST["code"];
-$input=$_POST["input"];
-$code_file="main.c";
-$error_file="error.txt";
-$input_file="input.txt";
-$gcc="gcc";
-$executable="a.out";
-$output= "timesout 5s ./a.out";
-$cmd=$gcc." -lm ".$code_file;
-$cmderror=$cmd." &> ".$error_file;
-$check=0;
-$file_code=fopen($code_file,"w+");
-fwrite($file_code, $code);
-fclose($file_code);
-exec("chmod 777 $executable");
-exec("chmod 777 $error_file");
-shell_exec($cmderror);
-$error=file_get_contents($error_file);
-$startTime=microtime(true);
-if (trim($error)=="") {
-	if(trim($input)==""){
-		$out=shell_exec($output);
-	}
-	else{
-		$output=$output." < ".$input_file;
-		$out=shell_exec($output);
-	}
-	echo "<pre>$out</pre>";
-}
-else if(!strpos($error,"error")) {
-	echo "<pre>$error</pre>";
-	if(trim($input)=="")
+<?php
+	$CC="gcc";
+	$out="timeout 5s ./a.out";
+	$code=$_POST["code"];
+	$input=$_POST["input"];
+	$filename_code="main.c";
+	$filename_in="input.txt";
+	$filename_error="error.txt";
+	$executable="a.out";
+	$command=$CC." -lm ".$filename_code;	
+	$command_error=$command." 2>".$filename_error;
+	$check=0;
+
+	//if(trim($code)=="")
+	//die("The code area is empty");
+	
+	$file_code=fopen($filename_code,"w+");
+	fwrite($file_code,$code);
+	fclose($file_code);
+	$file_in=fopen($filename_in,"w+");
+	fwrite($file_in,$input);
+	fclose($file_in);
+	exec("chmod 777 $executable"); 
+	exec("chmod 777 $filename_error");	
+
+	shell_exec($command_error);
+	$error=file_get_contents($filename_error);
+	$executionStartTime = microtime(true);
+
+	if(trim($error)=="")
 	{
-		$out=shell_exec($output);
+		if(trim($input)=="")
+		{
+			$output=shell_exec($out);
+		}
+		else
+		{
+			$out=$out." < ".$filename_in;
+			$output=shell_exec($out);
+		}
+		//echo "<pre>$output</pre>";
+        echo "<textarea id='div' class=\"form-control\" name=\"output\" rows=\"10\" cols=\"50\">$output</textarea><br><br>";
 	}
-	else{
-		$output=$output."<".$input_file;
-		$out=shell_exec($output);
+	else if(!strpos($error,"error"))
+	{
+		echo "<pre>$error</pre>";
+		if(trim($input)=="")
+		{
+			$output=shell_exec($out);
+		}
+		else
+		{
+			$out=$out." < ".$filename_in;
+			$output=shell_exec($out);
+		}
+		//echo "<pre>$output</pre>";
+                echo "<textarea id='div' class=\"form-control\" name=\"output\" rows=\"10\" cols=\"50\">$output</textarea><br><br>";
 	}
-	echo "<pre>$out</pre>";
-}
-else{
-	echo "<pre>$error</pre>";
-	$check=1;
-}
-$endTime=microtime(true);
-$sec=$endTime-$startTime;
-$sec=sprintf("%0.2f",$sec);
-echo "<pre>Time taken: $sec</pre>";
-if($check==1){
-	echo "<pre>Compilation Error (CE)</pre>";
-}
-else if($check==0 && $sec>3)
-{
-	echo "<pre>Time Limit Exceeded (TLE)</pre>";
-}
-else if(trim($out)=="")
-{
-	echo "<pre>Wrong Answer(WA)</pre>";
-}
-else if($check==0)
-{
-	echo "<pre>All Correct (AC)</pre>";
-}
-exec("rm $code_file");
-exec("rm *.o");
-exec("rm *.txt");
-exec("rm $executable");
+	else
+	{
+		echo "<pre>$error</pre>";
+		$check=1;
+	}
+	$executionEndTime = microtime(true);
+	$seconds = $executionEndTime - $executionStartTime;
+	$seconds = sprintf('%0.2f', $seconds);
+	echo "<pre>Compiled And Executed In: $seconds s</pre>";
+	if($check==1)
+	{
+		echo "<pre>Verdict : CE</pre>";
+	}
+	else if($check==0 && $seconds>3)
+	{
+		echo "<pre>Verdict : TLE</pre>";
+	}
+	else if(trim($output)=="")
+	{
+		echo "<pre>Verdict : WA</pre>";
+	}
+	else if($check==0)
+	{
+		echo "<pre>Verdict : AC</pre>";
+	}
+	exec("rm $filename_code");
+	exec("rm *.o");
+	exec("rm *.txt");
+	exec("rm $executable");
 ?>
