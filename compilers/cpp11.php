b93bfb8ece92c047c3c87cea8c37a3b8c133f57d
ayush115 <33548141+ayush115@users.noreply.github.com>
2018-09-20 20:54:04 +0530

    Update cpp11.php

--- compilers/cpp11.php
+++ compilers/cpp11.php
@@ -1,70 +1,77 @@
-<?php 
+<?php
 $code=$_POST["code"];
 $input=$_POST["input"];
-$code_file="main.cpp";
-$error_file="error.txt";
+$code_file="main.c";
 $input_file="input.txt";
+$error_file="error.txt";
 $gpp="g++ --std=c++11";
+$out="timeout 5s ./a.out";
 $executable="a.out";
-$output= "timesout 5s ./a.out";
-$cmd=$gpp."-lm".$code_file;
-$cmderror=$cmd." &> ".$error_file;
-$check=0;
+$cmd=$gpp." -lm ".$code_file;	
+$cmd_error=$cmd." 2>".$error_file;
+$flag=0;
 $file_code=fopen($code_file,"w+");
-fwrite($file_code, $code);
+fwrite($file_code,$code);
 fclose($file_code);
 $file_input=fopen($input_file,"w+");
-fwrite($file_input, $input);
+fwrite($file_input,$input);
 fclose($file_input);
-exec("chmod 777 $executable");
-exec("chmod 777 $error_file");
-shell_exec($cmderror);
+exec("chmod 777 $executable"); 
+exec("chmod 777 $error_file");	
+shell_exec($cmd_error);
 $error=file_get_contents($error_file);
-$startTime=microtime(true);
-if (trim($error)=="") {
-	if(trim($input)==""){
-		$out=shell_exec($output);
+$startTime = microtime(true);
+if(trim($error)=="")
+{
+	if(trim($input)=="")
+	{
+		$output=shell_exec($out);
 	}
-	else{
-		$output=$output."< ".$input_file;
-		$out=shell_exec($output);
+	else
+	{
+		$out=$out." < ".$input_file;
+		$output=shell_exec($out);
 	}
-	echo "<pre>$out</pre>";
+	echo "<pre>$output</pre>";
 }
-else if(!strpos($error,"error")) {
+else if(!strpos($error,"error"))
+{
 	echo "<pre>$error</pre>";
 	if(trim($input)=="")
 	{
-		$out=shell_exec($output);
+		$output=shell_exec($out);
 	}
-	else{
-		$output=$output."<".$input_file;
-		$out=shell_exec($output);
+	else
+	{
+		$out=$out." < ".$input_file;
+		$output=shell_exec($out);
 	}
-	echo "<pre>$out</pre>";
+	echo "<pre>$output</pre>";
 }
-else{
+else
+{
 	echo "<pre>$error</pre>";
-	$check=1;
+	$flag=1;
 }
-$endTime=microtime(true);
-$sec=$endTime-$startTime;
-$sec=printf("%0.2f",$sec);
-echo "<pre>Time taken: $sec</pre>";
-if($check==1){
-	echo "<pre>Compilation Error (CE)</pre>";
-}
-else if($check==0 && $seconds>3)
+$endTime = microtime(true);
+$time = $endTime - $startTime;
+$time = sprintf('%0.2f', $time);
+echo "<pre>Time taken : $time s</pre>";
+if($flag==1)
 {
-	echo "<pre>Time Limit Exceeded (TLE)</pre>";
+	echo "<pre>Compilation Error</pre>";
 }
-else if(trim($out)=="")
+else if($flag==0 && $time>3)
 {
-	echo "<pre>Wrong Answer(WA)</pre>";
+	echo "<pre>Time Limit Exceeded</pre>";
 }
-else if($check==0)
+else if(trim($output)=="")
 {
-	echo "<pre>All Correct (AC)</pre>";
+	echo "<pre>Wrong Answer</pre>";
+}
+else if($flag==0)
+{
+	echo "<pre>Success</pre>";
 }
 exec("rm $code_file");
 exec("rm *.o");
