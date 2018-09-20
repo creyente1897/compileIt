4a5b223fe5f77bbbabb9ae2b913dd80de691a450
ayush115 <33548141+ayush115@users.noreply.github.com>
2018-09-20 21:19:09 +0530

    Update java.php

--- compilers/java.php
+++ compilers/java.php
@@ -7,11 +7,10 @@ $input_file="input.txt";
 $runtime_file="runtime.txt";
 $java="javac";
 $executable="*.class";
-$output= "timesout 6s java Main";
+$out= "timesout 6s java Main";
 $cmd=$java." ".$code_file;
-$cmderror=$cmd." 2> ".$error_file;
-$cmdrunerror=$output." 2> ".$runtime_file;
-
+$cmderror=$cmd." 2>".$error_file;
+$cmdrunerror=$out." 2>".$runtime_file;
 $file_code=fopen($code_file,"w+");
 fwrite($file_code, $code);
 fclose($file_code);
@@ -23,53 +22,51 @@ exec("chmod 777 $error_file");
 shell_exec($cmderror);
 $error=file_get_contents($error_file);
 $startTime=microtime(true);
-if (trim($error)=="") {
-	if(trim($input)==""){
+if (trim($error)=="") 
+{
+	if(trim($input)=="")
+	{
 		shell_exec($cmdrunerror);
 		$runtime_error=file_get_contents($runtime_file);
-		$out=shell_exec($output);
+		$output=shell_exec($out);
 	}
 	else{
 		shell_exec($cmdrunerror);
 		$runtime_error=file_get_contents($runtime_file);
-		$output=$output." < ".$input_file;
-		$out=shell_exec($output);
+		$out=$out." < ".$input_file;
+		$output=shell_exec($out);
 	}
-	echo "<pre>$out</pre>";
+	echo "<pre>$output</pre>";
 }
-else if(!strpos($error,"error")) {
+else if(!strpos($error,"error"))
+{
 	echo "<pre>$error</pre>";
 	if(trim($input)=="")
 	{
-		$out=shell_exec($output);
+		$output=shell_exec($out);
 	}
-	else{
-		$output=$output."<".$input_file;
-		$out=shell_exec($output);
+	else
+	{
+		$out=$out."<".$input_file;
+		$output=shell_exec($out);
 	}
-	echo "<pre>$out</pre>";
+	echo "<pre>$output</pre>";
 }
-else{
+else
+{
 	echo "<pre>$error</pre>";
 }
 $endTime=microtime(true);
 $sec=$endTime-$startTime;
 $sec=sprintf("%0.2f",$sec);
-echo "<pre>Time taken: $sec</pre>";
-/*if($check==1){
-	echo "<pre>Compilation Error (CE)</pre>";
-}*/
+echo "<pre>Time taken: $sec s</pre>";
 if($sec>5)
 {
-	echo "<pre>Time Limit Exceeded (TLE)</pre>";
+	echo "<pre>Time Limit Exceeded</pre>";
 }
-/*else if(trim($out)=="")
-{
-	echo "<pre>Wrong Answer(WA)</pre>";
-}*/
 else
 {
-	echo "<pre>All Correct (AC)</pre>";
+	echo "<pre>Success</pre>";
 }
 exec("rm $code_file");
 exec("rm *.java");
